c119f990063334b9a6e8d92dc955b1897eb205dc
briannadrew <briannadrew@trentu.ca>
2019-02-05 22:56:10 -0500

    commit

---
+++ Brianna_Drew_Assignment/Brianna_Drew_Assignment(3)/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.salesReceipt = New System.Windows.Forms.ListBox()
+        Me.itemBox = New System.Windows.Forms.TextBox()
+        Me.priceBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.addItem = New System.Windows.Forms.Button()
+        Me.endSale = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.resetButton = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'salesReceipt
+        '
+        Me.salesReceipt.Font = New System.Drawing.Font("Courier New", 12.5!)
+        Me.salesReceipt.FormattingEnabled = True
+        Me.salesReceipt.ItemHeight = 23
+        Me.salesReceipt.Location = New System.Drawing.Point(13, 61)
+        Me.salesReceipt.Name = "salesReceipt"
+        Me.salesReceipt.Size = New System.Drawing.Size(540, 671)
+        Me.salesReceipt.TabIndex = 0
+        '
+        'itemBox
+        '
+        Me.itemBox.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.itemBox.Font = New System.Drawing.Font("Courier New", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.itemBox.Location = New System.Drawing.Point(807, 228)
+        Me.itemBox.Multiline = True
+        Me.itemBox.Name = "itemBox"
+        Me.itemBox.Size = New System.Drawing.Size(561, 83)
+        Me.itemBox.TabIndex = 1
+        '
+        'priceBox
+        '
+        Me.priceBox.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.priceBox.Font = New System.Drawing.Font("Courier New", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.priceBox.Location = New System.Drawing.Point(807, 330)
+        Me.priceBox.Multiline = True
+        Me.priceBox.Name = "priceBox"
+        Me.priceBox.Size = New System.Drawing.Size(561, 83)
+        Me.priceBox.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("OCR A Extended", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(592, 253)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 32)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Item Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("OCR A Extended", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(664, 357)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 32)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Price:"
+        '
+        'addItem
+        '
+        Me.addItem.Location = New System.Drawing.Point(934, 437)
+        Me.addItem.Name = "addItem"
+        Me.addItem.Size = New System.Drawing.Size(322, 54)
+        Me.addItem.TabIndex = 5
+        Me.addItem.Text = "Add Item to Reciept"
+        Me.addItem.UseVisualStyleBackColor = True
+        '
+        'endSale
+        '
+        Me.endSale.Location = New System.Drawing.Point(934, 510)
+        Me.endSale.Name = "endSale"
+        Me.endSale.Size = New System.Drawing.Size(322, 54)
+        Me.endSale.TabIndex = 6
+        Me.endSale.Text = "End Transaction"
+        Me.endSale.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(1215, 679)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(153, 62)
+        Me.exitButton.TabIndex = 8
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("OCR A Extended", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(117, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(295, 35)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Sales Receipt:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Brianna_Drew_Assignment_3_.My.Resources.Resources._95f81b33_e762_4bbd_847f_0245c54e59eb
+        Me.PictureBox1.Location = New System.Drawing.Point(670, -55)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(797, 357)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = Global.Brianna_Drew_Assignment_3_.My.Resources.Resources.ba336b54e44ba2554b3aa272960585fc
+        Me.PictureBox2.Location = New System.Drawing.Point(580, 442)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(618, 352)
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'resetButton
+        '
+        Me.resetButton.Location = New System.Drawing.Point(840, 679)
+        Me.resetButton.Name = "resetButton"
+        Me.resetButton.Size = New System.Drawing.Size(153, 62)
+        Me.resetButton.TabIndex = 12
+        Me.resetButton.Text = "Reset"
+        Me.resetButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1419, 773)
+        Me.Controls.Add(Me.resetButton)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.endSale)
+        Me.Controls.Add(Me.addItem)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.priceBox)
+        Me.Controls.Add(Me.itemBox)
+        Me.Controls.Add(Me.salesReceipt)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Brianna's Rockin' Records Transaction/Receipt Processor"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents salesReceipt As ListBox
+    Friend WithEvents itemBox As TextBox
+    Friend WithEvents priceBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents addItem As Button
+    Friend WithEvents endSale As Button
+    Friend WithEvents exitButton As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents resetButton As Button
+End Class
---
+++ Brianna_Drew_Assignment/Brianna_Drew_Assignment(3)/Form1.vb
@@ -0,0 +1,109 @@
+﻿'
+' This is a form that performs as a basic sales receipt producer for a store.
+'
+' Written by: Brianna Drew (0622446) - March 2018
+'
+' Purpose: To produce a properly formatted sales receipt for a store based on input.
+'
+' Parameters: None.
+'
+' Subroutine Required: The form itself loading has a subroutine as well as each button on the form has it's own subroutine.
+'
+' Modifications: - March 3/18 - Program started, basic features dragged to form (buttons, text box, label) as well as almost the entire
+'                               program written (except for coding of the reset button).
+'                - March 4/18 - Reset button and it's code added, some comments added.
+'                - March 6/18 - Rest of comments added.
+'
+
+Public Class Form1
+
+
+    ' This section of code defines the variables required (at class-level).
+    Dim fmtStr As String = "{0, 18}{1, 18}" ' Definition for variable that holds the format for character padding for elements on the receipt.
+    Dim itemCount As Integer = 0 ' Definition for variable to hold the number of items added to the receipt.
+    Dim totalCost As Double = 0.0 ' Definition for variable to hold the total cost of items in the transaction before tax.
+    Dim taxCalc As Double = 0.0 ' Definition for variable to hold the calculated tax to be added to the total cost of the transaction.
+    Dim finalCost As Double = 0.0 ' Definition for variable to hold the total cost of items in the transaction including tax.
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' This subroutine runs when the form (program) first loads.
+
+        salesReceipt.Items.Add(" Welcome to Brianna's Rockin' Records!") ' Add a "welcome" statement to the receipt (list box).
+        salesReceipt.Items.Add(" ") ' Add blank line to receipt (list box) to format.
+        salesReceipt.Items.Add("         " & DateAndTime.Now.ToString) ' Add the current date and time to the receipt (list box).
+        salesReceipt.Items.Add(" ") ' Add blank line to receipt (list box) to format.
+
+    End Sub
+
+
+    Private Sub addItem_Click(sender As Object, e As EventArgs) Handles addItem.Click
+        ' This subroutine runs when the "add item to receipt" button is clicked.
+
+        If Not IsNumeric(priceBox.Text) Then ' If what was entered as the item price is not a numerical value, then...
+            MessageBox.Show("Invalid Price Format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation) ' A small window appears showing the
+            ' error along with a ok button and exclamation icon.
+        Else ' If what was entered as the item price is a numerical value, then...
+            salesReceipt.Items.Add(String.Format(fmtStr, itemBox.Text, FormatCurrency(priceBox.Text))) ' Adds the item name and price entered to the
+            ' receipt (list box) formatted with the character padding, as well as the price formatted as currency.
+            totalCost += priceBox.Text ' Adds the price of item entered to the sum of the item prices.
+            itemCount += 1 ' Number of items increases by 1.
+        End If
+
+        itemBox.Clear() ' Clear the item entered from the box in which it was inputted.
+        priceBox.Clear() ' Clear the item price entered from the box in which it was inputted.
+
+    End Sub
+
+
+    Private Sub endSale_Click(sender As Object, e As EventArgs) Handles endSale.Click
+        ' This subroutine runs when the "end transaction" button is clicked.
+
+        endSale.Enabled = False ' Disables the "end transaction" button.
+        addItem.Enabled = False ' Disables the "add item to receipt" button.
+        salesReceipt.Items.Add(String.Format(fmtStr, "---------------", "----------")) ' Adds dotted line to seperate items and calculations to the
+        ' receipt (list box) formatted with the character padding.
+        salesReceipt.Items.Add(String.Format(fmtStr, "Cost of Items: ", (FormatCurrency(totalCost)))) ' Adds the total cost of items (before tax) to
+        ' the receipt (list box) formatted with the character padding and with the actual value formatted as a currency.
+        salesReceipt.Items.Add(String.Format(fmtStr, "Number of Items: ", itemCount)) ' Adds the total number of items purchased to the receipt
+        ' (list box) formatted with the character padding.
+        taxCalc = totalCost * 0.13 ' Calculates the amount of tax to be added to the cost of items and assigns that value to the designated variable.
+        salesReceipt.Items.Add(String.Format(fmtStr, "Taxes: ", (FormatCurrency(taxCalc)))) ' Adds the tax to be added to the cost of items to the
+        ' receipt (list box) formatted with the character padding and with the actual value formatted as a currency.
+        finalCost = totalCost + taxCalc ' Calculates the final cost by adding the tax to the cost of items and assigns that value to the designated variable.
+        salesReceipt.Items.Add(String.Format(fmtStr, "Total Cost: ", (FormatCurrency(finalCost)))) ' Adds the total cost (including tax) to the receipt
+        ' (list box) formatted with the character padding and with the actual value formatted as a currency.
+        salesReceipt.Items.Add(" ") ' Add blank line to receipt (list box) to format.
+        salesReceipt.Items.Add("Thank you for shopping with us!") ' Add a "farewell" statement to the receipt (list box).
+        salesReceipt.Items.Add("Come back again soon!") ' Add a "farewell" statement to the receipt (list box).
+
+    End Sub
+
+
+    Private Sub resetButton_Click(sender As Object, e As EventArgs) Handles resetButton.Click
+        ' This subroutine runs when the "reset" button is clicked.
+
+        endSale.Enabled = True ' Re-enables the "end transaction" button.
+        addItem.Enabled = True ' Re-enables the "add item to receipt" button.
+        salesReceipt.Items.Clear() ' Clears everything from the receipt (list box).
+        itemCount = 0 ' Resets variable that holds the number of items added to the receipt to 0.
+        totalCost = 0.0 ' Resets variable that holds the total cost of items in the transaction before tax to 0.
+        taxCalc = 0.0 ' Resets variable that holds the calculated tax to be added to the total cost of the transaction to 0.
+        finalCost = 0.0 ' Resets variable that holds the total cost of items in the transaction including tax to 0.
+        salesReceipt.Items.Add(" Welcome to Brianna's Rockin' Records!") ' Add a "welcome" statement to the receipt (list box).
+        salesReceipt.Items.Add(" ") ' Add blank line to receipt (list box) to format.
+        salesReceipt.Items.Add("         " & DateAndTime.Now.ToString) ' Add the current date and time to the receipt (list box).
+        salesReceipt.Items.Add(" ") ' Add blank line to receipt (list box) to format.
+
+    End Sub
+
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        ' This subroutine runs when the "exit" button is clicked.
+
+        Me.Close() ' Program closes.
+
+    End Sub
+
+
+End Class
